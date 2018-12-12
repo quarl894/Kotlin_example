afb3237db4c75c03630ca45d72e1590ee3ba4304
DESKTOP-2KTFD9E\HANILN <quarl894@gmail.com>
2018-12-12 16:53:02 +0900

    Revert "Kotlin apply"
    This reverts commit 561b3837416e7d13000d5948198f6e77fc56a25d.

--- app/src/main/java/com/example/youngjung/kotiln_example/ui/repository_List/RepositoriesListActivity.kt
+++ app/src/main/java/com/example/youngjung/kotiln_example/ui/repository_List/RepositoriesListActivity.kt
@@ -2,67 +2,25 @@ package com.example.youngjung.kotiln_example.ui.repository_List
 
 import android.content.Context
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
-import android.view.View
-import client.yalantis.com.githubclient.flow.repository.RepositoryDetailActivity
 import client.yalantis.com.githubclient.model.Repository
 import com.example.youngjung.kotiln_example.R
 import com.example.youngjung.kotiln_example.ui.base.baseActivity
-import kotlinx.android.synthetic.main.activity_organization.*
-import java.util.ArrayList
 
 class RepositoriesListActivity : baseActivity<RepositoriesContract.View,
         RepositoriesPresenter>(),
-        RepositoriesContract.View {
-
-    private var mAdapter: RepositoriesAdapter? = null
-    override var mPresenter: RepositoriesPresenter = RepositoriesPresenter()
-
-    override fun showOrganizations(repositories: MutableList<Repository>) {
-        mAdapter?.addRepositories(repositories)
-        mAdapter?.notifyDataSetChanged()
-        hideProgress()
-    }
+        RepositoriesContract.View  {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_organization)
-        setUpRecyclerView()
-        mPresenter.loadRepositories()
-
-        toolbar.title = getString(R.string.title_activity_repositories)
-        showProgress()
-        fab.setOnClickListener {
-            showProgress()
-            mPresenter.loadRepositories()
-        }
+        setContentView(R.layout.activity_main)
     }
 
-    private fun setUpRecyclerView() {
-        mAdapter = RepositoriesAdapter(ArrayList<Repository>(), {
-            startActivity(RepositoryDetailActivity.newIntent(this, it.name))
-        })
-        recyclerViewRepositories.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        recyclerViewRepositories.adapter = mAdapter
+    override fun showOrganizations(repositories: MutableList<Repository>) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    private fun showProgress() {
-        recyclerViewRepositories.visibility = View.GONE
-        progress_bar.visibility = View.VISIBLE
-        fab.isEnabled = false
-    }
+    override var mPresenter: RepositoriesPresenter
+        get() = TODO("not implemented") //To change initializer of created properties use File | Settings | File Templates.
+        set(value) {}
 
-    private fun hideProgress() {
-        recyclerViewRepositories.visibility = View.VISIBLE
-        progress_bar.visibility = View.GONE
-        fab.isEnabled = true
-    }
-
-    override fun showError(error: String?) {
-        super.showError(error)
-        progress_bar.visibility = View.GONE
-        fab.isEnabled = true
-    }
 }
-
-
--- app/src/main/java/com/example/youngjung/kotiln_example/ui/repository_List/RepositoriesPresenter.kt
+++ app/src/main/java/com/example/youngjung/kotiln_example/ui/repository_List/RepositoriesPresenter.kt
@@ -1,9 +1,6 @@
 package com.example.youngjung.kotiln_example.ui.repository_List
 
-import com.example.youngjung.kotiln_example.api.GeneralErrorHandler
-import com.example.youngjung.kotiln_example.manager.ApiManager
 import com.example.youngjung.kotiln_example.ui.base.basePresenterlmpl
-import rx.functions.Action1
 
 class RepositoriesPresenter : basePresenterlmpl<RepositoriesContract.View>(), RepositoriesContract.Presenter {
 
@@ -13,9 +10,6 @@ class RepositoriesPresenter : basePresenterlmpl<RepositoriesContract.View>(), Re
     }
 
     override fun loadRepositories() {
-        ApiManager.loadOrganizationRepos(ORGANIZATION_NAME, REPOS_TYPE)
-                .doOnError { mView?.showMessage(it.toString()) }
-                .subscribe(Action1 { mView?.showOrganizations(it) },
-                        GeneralErrorHandler(mView, true, { throwable, errorBody, isNetwork -> mView?.showError(throwable.message) }))
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
